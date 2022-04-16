d24f5504246087eb4b2257e6df05d113cd50bb5c
Masen Furer <m_github@0x26.net>
2022-04-16 00:18:11 -0700

    update service dependencies and startup order
    clean up bindings and stack restartability
    
    should run cleanly on fresh and old nodes

--- roles/node/files/build_node.sh
+++ roles/node/files/build_node.sh
@@ -6,5 +6,6 @@ cd /opt/node/node-*
 gzip -d ../node_*.diff.gz --stdout | patch -p1
 ./configure
 make -j4
-make install
+# specifically avoiding `installconf` since it overwrites ansible's config
+make installbin installhelp installman
 gzip -d ../node_*.diff.gz --stdout | patch -p1 -R
